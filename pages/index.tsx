0b2500ae7fb64eeba2b144dad1dd1b070961ff47
Mantavya Maan <151978858+mantavyamaan@users.noreply.github.com>
2025-07-14 10:20:40 +0530

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center min-h-screen bg-black-100">
+      <h1 className="text-3xl font-bold text-center">Focusmate MVP</h1>
+    </main>
+  );
+}
+// This is a simple Next.js page that uses the Geist font.
